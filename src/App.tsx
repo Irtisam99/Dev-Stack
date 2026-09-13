16651d5aa0bb78b6a7bc6fa424f595b06235f2ef
Irtisam Alavi <faruqui500@gmail.com>
2026-09-13 17:37:52 +0600

    JSON Data entry and fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
 import Nav from './Nav'
 import Hero from './Hero'
+import type { ITechnology } from './types/technologyType'
+
+
+const technologiesFetch=async():Promise<ITechnology[]>=>{
+  const res=await fetch('/data.json');
+  const data=await res.json();
+  return data
+}
 
 function App() {
-
+  const[technologyPromise]=useState(()=>technologiesFetch())
+  console.log(technologyPromise)
 
   return (
     <>
